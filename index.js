1ab359027153bebad38465f65d0283d34ae4de05
Pedro Augusto <pedroaugdev@gmail.com>
2023-09-15 10:34:58 -0300

    Adicionando conteudos do bem-vindo e criando pastas para organizar

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
+const handlebars = require('express-handlebars')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
-    res.render('index')
+    const titulo = 'Inicio'
+    
+    res.render('index', {titulo: titulo})
 })
 
 app.post('/', (req, res)=>{
-    const nome = req.body.nome
-    const idade = req.body.idade
+    const variaveis = 
+    {   
+        nome: req.body.nome, 
+        idade: req.body.idade, 
+        titulo: 'Bem-Vindo', 
+        cs2: 'images/cs2.png', 
+        tsLogo: 'images/tsLogo.png', 
+        jsLogo: 'images/jsLogo.png'
+    }
 
-    res.render('bemvindo', {nome: nome, idade: idade})
+    res.render('bemvindo', variaveis)
 })
 
 app.listen(3000, function(erro){
